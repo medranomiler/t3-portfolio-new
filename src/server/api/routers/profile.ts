543bd15dc2e339953fbfa8ceec672286c2b15c83
Medranomiler1 <medranomiler@gmail.com>
2024-03-10 23:48:05 -0500

    updates to post and profile routers

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -7,18 +7,28 @@ import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 export const profileRouter = createTRPCRouter({
   getUserByUsername: publicProcedure
-    .input(z.object({ userId: z.string() }))
+    .input(z.object({ username: z.string() }))
     .query(async ({ input }) => {
       const [user] = await clerkClient.users.getUserList({
-        userId: [input.userId]
+        username: [input.username]
       });
 
       if (!user) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "User not found",
-        });
+        const users = (
+          await clerkClient.users.getUserList({
+            limit: 200,
+          })
+        )
+        const user = users.find((user) => user.externalAccounts.find((account) => account.username === input.username));
+        if (!user) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "User not found",
+          });
+        }
+        return filterUserForClient(user)
       }
-      return filterUserForClient(user)
+
+      return filterUserForClient(user);
     }),
 });
