da897d5d50a5051b2bd997fbe8a8c5e0744a0e1c
Medranomiler1 <medranomiler@gmail.com>
2024-03-10 23:48:29 -0500

    updates to home and profile pages

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,18 +1,54 @@
-import { unstable_noStore as noStore } from "next/cache";
-import { api } from "~/trpc/server";
+import Head from "next/head";
+import { api } from "~/trpc/server"
+import { PostView } from "../_components/postview"
 
-
-export default async function Profile({ params }: { params: { slug: string } }) {
-  noStore();
-  // const userHandle = params.slug.replace("@", "")
-  const data = await api.profile.getUserByUsername.query({userId: params.slug})
-  
+const ProfileFeed = async (props: { userId: string }) => {
+  const data = await api.post.getPostsByUserId.query({
+    userId: props.userId,
+  });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-            {data.id}
-      </div>
-    </main>
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
   );
-}
+};
+
+export default async function ProfilePage({ params }: { params: { slug: string } }){
+  const username = decodeURIComponent(params.slug).replace("@", "")
+  console.log({username})
+  const data = await api.profile.getUserByUsername.query({
+    username,
+  });
+
+
+  if (!data) return <div>404</div>;
+  return (
+    <>
+      <Head>
+        <title>{data.username}</title>
+      </Head>
+      
+        <div className="relative h-36 bg-slate-600">
+          <img
+            src={data.imageUrl}
+            alt={`${
+              data.username ?? "unknown"
+            }'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? "unknown"
+        }`}</div>
+        <div className="w-full border-b border-slate-400" />
+        <ProfileFeed userId={data.id} />
+      
+    </>
+  );
+};
