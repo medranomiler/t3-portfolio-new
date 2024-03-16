04e925a1ea2f5d2bb395d89b2615ed9fca401a52
Medranomiler1 <medranomiler@gmail.com>
2024-03-16 00:07:27 -0500

    linting temp fixes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import { api } from "~/trpc/server"
 import { PostView } from "../_components/postview"
--- src/app/_components/postview.tsx
+++ src/app/_components/postview.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import type { RouterOutputs } from "~/trpc/shared";
 
 type PostWithUser = RouterOutputs["post"]["getAll"][number]
