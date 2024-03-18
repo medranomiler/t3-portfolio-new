7fef9a435de738625f1159e3d19e7edf7a68f84a
Medranomiler1 <medranomiler@gmail.com>
2024-03-17 21:53:04 -0500

    Revert "linting temp fixes"
    This reverts commit 04e925a1ea2f5d2bb395d89b2615ed9fca401a52.

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import { api } from "~/trpc/server"
 import { PostView } from "../_components/postview"
--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,6 +1,5 @@
 "use client";
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
+
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
